3ae15fe4ffd2d3964860b35f6af808bb7e94bb13
nikitaWebApps <nikitagolovlev.design@gmail.com>
2024-06-14 16:46:23 +0500

    major code refactoring and duplicate code removal

--- src/components/resumeTabs.vue
+++ src/components/resumeTabs.vue
@@ -1,83 +1,61 @@
-<script setup>
+<script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import { currentSiteLanguage } from '../langStore';
 import { useStore } from '@nanostores/vue';
 
 const $currentSiteLanguage = useStore(currentSiteLanguage)
 
-const tabsRu = [
-    'Опыт работы',
-    'Навыки',
-    'Образование',
-    'Обо мне'
-]
+const tabs = {
+    ru: [
+        'Опыт работы',
+        'Навыки',
+        'Образование',
+        'Обо мне'
+    ],
+    en: [
+        'Work Experience',
+        'Skills',
+        'Education',
+        'About'
+    ]
+}
 
-const tabsEng = [
-    'Work Experience',
-    'Skills',
-    'Education',
-    'About'
-]
-
-const screenWidth = ref()
+const screenWidth = ref<number>(0)
 onMounted(() => {
-    if (currentSiteLanguage.get() == 'ru') {
-        activeTab.value['Опыт работы'] = true
-    } else {
-        activeTab.value['Work Experience'] = true
-    }
+    activeTab.value[tabs[currentSiteLanguage.get()][0]] = true
     screenWidth.value = window.innerWidth
     window.addEventListener('resize', () => {
         screenWidth.value = window.innerWidth
     })
 })
 
-const activeTab = ref({})
-function handleTabClick(tab, event) {
+const activeTab = ref<{ [key: string]: boolean }>({})
+
+function handleTabClick(tabIndex: number, event: Event) {
     activeTab.value = {}
-    activeTab.value[tab] = true
-    event.target.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' })
+    activeTab.value[tabs[currentSiteLanguage.get()][tabIndex]] = true
+    const targetElement = event.target as HTMLButtonElement
+    targetElement.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' })
 }
 
 
 </script>
 
 <template>
-    <div v-if="$currentSiteLanguage == 'ru'" class="">
-        <nav v-if="screenWidth < 1024"
-            class='flex flex-row gap-3 px-4 sm:px-8 overflow-auto snap-x snap-proximity mb-6'>
-            <button v-for="(tab, index) in tabsRu"
-                :class="activeTab[tab] ? ['transition-colors duration-500 text-blue-600 border-blue-600 border-b-2'] : [' border-slate-500 text-slate-500']"
-                :data-tab="index + 1"
-                class="flex flex-row grow justify-center text-center text-nowrap py-2 px-2 text-lg snap-center transition-colors"
-                role="button"
-                @click="handleTabClick(tab, $event)"
-                :key="tab">{{ tab }}</button>
-        </nav>
-        <div :class="screenWidth >= 1024 ? ['grid grid-cols-2 gap-x-8 gap-y-16'] : ''" class="px-4 sm:px-8">
-            <slot v-if="activeTab['Опыт работы'] || screenWidth >= 1024" name="ru-work-exp-tab"> </slot>
-            <slot v-if="activeTab['Навыки'] || screenWidth >= 1024" name="ru-skills-tab"> </slot>
-            <slot v-if="activeTab['Образование'] || screenWidth >= 1024" name="ru-education-tab"> </slot>
-            <slot v-if="activeTab['Обо мне'] || screenWidth >= 1024" name="ru-about-tab"> </slot>
-        </div>
-    </div>
-
-    <div v-else class="">
-        <nav v-if="screenWidth < 1024"
-            class='flex flex-row gap-3 px-4 sm:px-8 overflow-auto snap-x snap-proximity mb-6'>
-            <button v-for="(tab, index) in tabsEng"
-                :class="activeTab[tab] ? ['transition-colors duration-500 text-blue-600 border-blue-600 border-b-2'] : [' border-slate-500 text-slate-500']"
-                :data-tab="index + 1"
-                class="flex flex-row grow justify-center text-center text-nowrap py-2 px-2 text-lg snap-center transition-colors"
-                role="button"
-                @click="handleTabClick(tab, $event)"
-                :key="tab">{{ tab }}</button>
-        </nav>
-        <div :class="screenWidth >= 1024 ? ['grid grid-cols-2 gap-x-8 gap-y-16'] : ''" class="px-4 sm:px-8">
-            <slot v-if="activeTab['Work Experience'] || screenWidth >= 1024" name="en-work-exp-tab"> </slot>
-            <slot v-if="activeTab['Skills'] || screenWidth >= 1024" name="en-skills-tab"> </slot>
-            <slot v-if="activeTab['Education'] || screenWidth >= 1024" name="en-education-tab"> </slot>
-            <slot v-if="activeTab['About'] || screenWidth >= 1024" name="en-about-tab"> </slot>
-        </div>
+    <nav v-if="screenWidth! < 1024"
+        class='flex flex-row gap-3 px-4 sm:px-8 overflow-auto snap-x snap-proximity mb-6'>
+        <button v-for="(tab, index) in tabs[$currentSiteLanguage]"
+            :class="activeTab[tab] ? ['transition-colors duration-500 text-blue-600 border-blue-600 border-b-2'] : [' border-slate-500 text-slate-500']"
+            :data-tab="index + 1"
+            class="flex flex-row grow justify-center text-center text-nowrap py-2 px-2 text-lg snap-center transition-colors"
+            role="button"
+            @click="handleTabClick(index, $event)"
+            :key="tab">{{ tab }}</button>
+    </nav>
+    <div :class="screenWidth! >= 1024 ? ['grid grid-cols-2 gap-x-8 gap-y-16'] : ''" class="px-4 sm:px-8">
+        <slot v-if="activeTab[tabs[$currentSiteLanguage][0]] || screenWidth >= 1024" name="work-exp-tab"> </slot>
+        <slot v-if="activeTab[tabs[$currentSiteLanguage][1]] || screenWidth >= 1024" name="skills-tab"> </slot>
+        <slot v-if="activeTab[tabs[$currentSiteLanguage][2]] || screenWidth >= 1024" name="education-tab"> </slot>
+        <slot v-if="activeTab[tabs[$currentSiteLanguage][3]] || screenWidth >= 1024" name="about-tab"> </slot>
     </div>
 </template>
---
+++ src/composables/useIsLangRu.ts
@@ -0,0 +1,4 @@
+export default function useIsLangRu(lang: 'ru' | 'en') {
+	return lang == 'ru'
+}
+
