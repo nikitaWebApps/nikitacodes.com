862badeb3c112e8c11c3c915f95016a3162ed23f
nikitaWebApps <nikitagolovlev.design@gmail.com>
2024-12-12 14:47:57 +0500

    add: venobox library for image preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,11 @@
 import { defineConfig } from 'astro/config'
 import vue from '@astrojs/vue'
 import tailwind from '@astrojs/tailwind'
-import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind(), mdx()],
+  integrations: [vue(), tailwind()],
   prefetch: true,
-  markdown: {
-    shikiConfig: {
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://shiki.style/languages
-      langs: ['javascript', 'typescript', 'php'],
-      // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-      // Add custom transformers: https://shiki.style/guide/transformers
-      // Find common transformers: https://shiki.style/packages/transformers
-      transformers: [],
-    },
-  },
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ru'],
