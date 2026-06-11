1861fcd6507e8d61a2afd61c19bba96aaa061469
nikitaWebApps <nikitagolovlev.design@gmail.com>
2026-06-11 22:53:31 +0200

    Add selected projects section

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -5,30 +5,33 @@ export const Languages = {
 
 export const defaultLang = 'en'
 
-export const translations = {
-    en: {
-        index_title: 'Nikita Golovlev, frontend-developer',
-        hello: 'Hello',
-        resume: 'CV',
-        links: 'Links',
-        focusArea: 'Focus Areas',
-        homepage: 'Homepage',
-        experience: 'Experience',
-        tab_skills: 'Skills',
-        education: 'Education',
-        higher_education: 'Education',
-        courses: 'Courses',
-        tab_summary: 'Summary',
-        open_pdf: 'Open PDF',
-        projectCard: {
-            details: 'Details',
-            see_live: 'See live',
-            inDevelopment: 'In deveplopment',
-        },
-        last_updated: 'Last updated',
-        next_slide: 'Next slide →',
-        prev_slide: '← Previous slide',
-        get_in_touch: 'Get in touch',
-        irina_designer: 'Irina Vasyuk',
+const en = {
+    index_title: 'Nikita Golovlev, frontend-developer',
+    hello: 'Hello',
+    resume: 'CV',
+    links: 'Links',
+    selectedProjects: 'Selected Projects',
+    homepage: 'Homepage',
+    experience: 'Experience',
+    tab_skills: 'Skills',
+    education: 'Education',
+    higher_education: 'Education',
+    courses: 'Courses',
+    tab_summary: 'Summary',
+    open_pdf: 'Open PDF',
+    projectCard: {
+        details: 'Details',
+        see_live: 'See live',
+        inDevelopment: 'In deveplopment',
     },
+    last_updated: 'Last updated',
+    next_slide: 'Next slide →',
+    prev_slide: '← Previous slide',
+    get_in_touch: 'Get in touch',
+    irina_designer: 'Irina Vasyuk',
+}
+
+export const translations = {
+    en,
+    ru: en,
 }
