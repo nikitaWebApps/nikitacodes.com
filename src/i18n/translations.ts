155045c9b0b5b4db545768f824fd62a6acc895b6
nikitaWebApps <nikitagolovlev.design@gmail.com>
2026-06-11 22:22:47 +0200

    Cleanup Resume and Education sections

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -1,34 +1,34 @@
 export const Languages = {
-  en: 'English',
-  ru: 'Russian',
+    en: 'English',
+    ru: 'Russian',
 }
 
 export const defaultLang = 'en'
 
 export const translations = {
-  en: {
-    index_title: 'Nikita Golovlev, frontend-developer',
-    hello: 'Hello',
-    resume: 'CV',
-    links: 'Links',
-    focusArea: 'Focus Areas',
-    homepage: 'Homepage',
-    experience: 'Experience',
-    tab_skills: 'Skills',
-    education: 'Education',
-    higher_education: 'Higher Education',
-    courses: 'Courses',
-    tab_summary: 'Summary',
-    open_pdf: 'Open PDF',
-    projectCard: {
-      details: 'Details',
-      see_live: 'See live',
-      inDevelopment: 'In deveplopment',
+    en: {
+        index_title: 'Nikita Golovlev, frontend-developer',
+        hello: 'Hello',
+        resume: 'CV',
+        links: 'Links',
+        focusArea: 'Focus Areas',
+        homepage: 'Homepage',
+        experience: 'Experience',
+        tab_skills: 'Skills',
+        education: 'Education',
+        higher_education: 'Education',
+        courses: 'Courses',
+        tab_summary: 'Summary',
+        open_pdf: 'Open PDF',
+        projectCard: {
+            details: 'Details',
+            see_live: 'See live',
+            inDevelopment: 'In deveplopment',
+        },
+        last_updated: 'Last updated',
+        next_slide: 'Next slide →',
+        prev_slide: '← Previous slide',
+        get_in_touch: 'Get in touch',
+        irina_designer: 'Irina Vasyuk',
     },
-    last_updated: 'Last updated',
-    next_slide: 'Next slide →',
-    prev_slide: '← Previous slide',
-    get_in_touch: 'Get in touch',
-    irina_designer: 'Irina Vasyuk',
-  },
 }
