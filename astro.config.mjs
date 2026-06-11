a677200781c89794ede9b6985e5f3297b2fcc5c0
nikitaWebApps <nikitagolovlev.design@gmail.com>
2026-06-11 22:48:47 +0200

    Fix tailwind installation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import {defineConfig} from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  prefetch: true,
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'ru'],
-    routing: {
-      prefixDefaultLocale: false,
+    prefetch: true,
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+        routing: {
+            prefixDefaultLocale: false,
+        },
     },
-  },
-  trailingSlash: 'ignore',
+    trailingSlash: 'ignore',
 })
-
